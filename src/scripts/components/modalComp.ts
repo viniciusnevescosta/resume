6e47c1b2f59bfb8e63e973dedafb59c675435930
viniciusnevescosta <vinicius.nevesco@gmail.com>
2025-02-27 11:14:31 -0300

    [wip] 🚧 Slots
    Slots's like function to add tagged content.

--- src/scripts/components/modalComp.ts
+++ src/scripts/components/modalComp.ts
@@ -6,25 +6,15 @@ if (!customElements.get('modal-comp')) {
 				super()
 
 				const TEMPLATE = document.createElement('template')
-
 				TEMPLATE.innerHTML = `
           <div id="container-popup--open" class="container-popup">
               <div class="popup">
                   <i id="close_modal" class="icon ri-close-line ri-xl" title="Fechar"></i>
                   <h3 class="popup-title">Título</h3>
                   <div class="popup-options">
-                    <form>
-                      <input type="radio" id="html" name="fav_language" value="HTML" checked="checked">
-                      <label for="html">HTML</label>
-                      
-                      <input type="radio" id="css" name="fav_language" value="CSS">
-                      <label for="css">CSS</label>
-                      
-                      <input type="radio" id="javascript" name="fav_language" value="JavaScript">
-                      <label for="javascript">JavaScript</label>
-                    </form>
+                    <form></form>
                   </div>
-                  <p class="popup-exit" title="Fechar" aria-label="Instrução de fechamento">
+                  <p class="popup-exit">
                     Pressione <kbd>ESC</kbd> para fechar
                   </p>
               </div>
@@ -32,7 +22,13 @@ if (!customElements.get('modal-comp')) {
           </div>
         `
 
-				this.appendChild(TEMPLATE.content.cloneNode(true))
+				const CLONE = TEMPLATE.content.cloneNode(true)
+				this.appendChild(CLONE)
+
+				const FORM = this.querySelector('form') as HTMLFormElement
+				while (this.firstChild) {
+					FORM.appendChild(this.firstChild)
+				}
 			}
 		},
 	)
