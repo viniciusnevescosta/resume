2cdba0d11d0ff260122578090b76ca8af53f623f
viniciusnevescosta <vinicius.nevesco@gmail.com>
2025-04-01 11:22:23 -0300

    [chore] 🔧 Astro PDF
    To generate PDF from homepage.

--- astro/astro.config.mjs
+++ astro/astro.config.mjs
@@ -4,10 +4,11 @@ import { defineConfig } from 'astro/config'
 import react from '@astrojs/react'
 import sitemap from '@astrojs/sitemap'
 import partytown from '@astrojs/partytown' // https://umami.is/
+import pdf from 'astro-pdf'
 
 // https://astro.build/config
 export default defineConfig({
-    integrations: [react(), sitemap(), partytown()],
+    integrations: [react(), sitemap(), partytown()], //  pdf() - https://ler.quest/astro-pdf/#other-configuration-options
     site: 'https://viniciusnevescosta.github.io',
-    base: './'
+    base: ''
 })
