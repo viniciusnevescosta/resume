63ad924fb4639bad3b713f66962224a4f8f7944c
viniciusnevescosta <vinicius.nevesco@gmail.com>
2025-04-01 09:29:29 -0300

    [feat] ✨ Sitemap and Partytown integrations

--- astro/astro.config.mjs
+++ astro/astro.config.mjs
@@ -2,8 +2,12 @@
 import { defineConfig } from 'astro/config'
 
 import react from '@astrojs/react'
+import sitemap from '@astrojs/sitemap'
+import partytown from '@astrojs/partytown' // https://umami.is/
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [react()],
+    integrations: [react(), sitemap(), partytown()],
+    site: 'https://viniciusnevescosta.github.io',
+    base: './'
 })
