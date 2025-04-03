cf228a0e898ecf859971613e9b44c481c8e010ad
Vinícius Neves Costa <vinicius.nevesco@gmail.com>
2025-04-03 14:11:40 +0000

    feat: React Support

---
+++ src/react/App.tsx
@@ -0,0 +1,76 @@
+import { useState } from 'react'
+import { BottomBar } from './shared/components/_BottomBar'
+import { OptionsMenu } from './shared/components/_Menu'
+import { OptionsModal } from './shared/components/_Modal'
+import { useTheme } from './shared/utils/useTheme'
+
+export const App = () => {
+    type ModalType = 'theme' | 'lang'
+    const [activeModal, setActiveModal] = useState<{
+        type: ModalType | null
+        isOpen: boolean
+    }>({ type: null, isOpen: false })
+
+    const { setTheme, effectiveTheme, theme } = useTheme()
+
+    const modalConfigs = {
+        theme: {
+            title: 'Selecionar tema',
+            options: [
+                {
+                    id: 'system-theme',
+                    value: 'Sistema',
+                    checked: theme === 'system-theme',
+                    onClick: () => setTheme('system-theme')
+                },
+                {
+                    id: 'light',
+                    value: 'Claro',
+                    checked: theme === 'light',
+                    onClick: () => setTheme('light')
+                },
+                {
+                    id: 'dark',
+                    value: 'Escuro',
+                    checked: theme === 'dark',
+                    onClick: () => setTheme('dark')
+                }
+            ]
+        },
+        lang: {
+            title: 'Selecionar idioma',
+            options: [
+                { id: 'en-us', value: 'Inglês' },
+                { id: 'es-es', value: 'Espanhol' },
+                { id: 'pt-br', value: 'Português' }
+            ]
+        }
+    }
+
+    const handleOpenModal = (type: ModalType): void => {
+        setActiveModal({ type, isOpen: true })
+    }
+
+    const handleCloseModal = (): void => {
+        setActiveModal({ type: null, isOpen: false })
+    }
+
+    return (
+        <>
+            <OptionsMenu
+                onClickModal={handleOpenModal}
+                Theme={effectiveTheme}
+            />
+            <BottomBar onClickModal={handleOpenModal} Theme={effectiveTheme} />
+
+            {activeModal.type && (
+                <OptionsModal
+                    modalTitle={modalConfigs[activeModal.type].title}
+                    modalOptions={modalConfigs[activeModal.type].options}
+                    isOpen={activeModal.isOpen}
+                    onClose={handleCloseModal}
+                />
+            )}
+        </>
+    )
+}
